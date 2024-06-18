03b8c17e943aadf93b358f315dcffad0965d2bfc
Christopher Leggett <chris@leggett.dev>
2024-06-17 20:39:11 -0400

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,6 +1,18 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+int handle_input(const char *input) {
+  char command[20];
+  sscanf(input, "%s", command);
+  if (!strcmp(command, "exit")) {
+    int arg = 0;
+    sscanf(input, "%*s %d", &arg);
+    exit(arg);
+  }
+  return 0;
+}
 
 int main() {
   while (true) {
@@ -12,7 +24,9 @@ int main() {
     fgets(input, 100, stdin);
 
     input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
+    if (!handle_input(input)) {
+      printf("%s: command not found\n", input);
+    }
   }
   return 0;
 }
