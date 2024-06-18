c32791e81b155658505af88eda08acea9c2a849a
Christopher Leggett <chris@leggett.dev>
2024-06-17 20:18:28 -0400

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,15 +1,18 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-  printf("$ ");
-  fflush(stdout);
+  while (true) {
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
