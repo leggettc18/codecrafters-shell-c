147fde4f37491d3b861c1fdb614dfe923d4d0140
Christopher Leggett <chris@leggett.dev>
2024-06-28 10:00:50 -0400

    Make structs for command and path

--- app/main.c
+++ app/main.c
@@ -6,27 +6,66 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+typedef struct {
+  char name[100];
+  char *args[100];
+  int num_args;
+} cmd_t;
+
+typedef struct {
+  char *paths[100];
+  int num_paths;
+} path_t;
+
+void parse_command(char *input, cmd_t *cmd) {
+  // Parse command name
+  for (int i = 0; i < strlen(input); i++) {
+    if (input[i] == ' ' || input[i] == '\n' || input[i] == '\0') {
+      cmd->name[i] = '\0';
+      break;
+    }
+    cmd->name[i] = input[i];
+  }
+  // Parse command arguments
+  char *args_str = input + strlen(cmd->name) + 1;
+  cmd->num_args = 0;
+  for (int i = 0; i < strlen(args_str); i++) {
+    if (args_str[i] == ' ' || args_str[i] == '\n' || args_str[i] == '\0') {
+      cmd->args[cmd->num_args] = (char *)malloc(i + 1);
+      strncpy(cmd->args[cmd->num_args], args_str, i);
+      cmd->args[cmd->num_args][i] = '\0';
+      cmd->num_args++;
+      args_str += i + 1;
+      i = 0;
+    }
+  }
+}
+
+void parse_path(path_t *path) {
+  char *path_str = getenv("PATH");
+  path->num_paths = 0;
+  for (int i = 0; i <= strlen(path_str); i++) {
+    if (path_str[i] == ':' || path_str[i] == '\0') {
+      path->paths[path->num_paths] = (char *)malloc(i + 1);
+      strncpy(path->paths[path->num_paths], path_str, i);
+      path->paths[path->num_paths][i] = '\0';
+      path->num_paths++;
+      path_str += i + 1;
+      i = 0;
+    }
+  }
+}
+
 int is_executable(const char *path) { return access(path, X_OK) == 0; }
 
-char *find_in_path(const char *command) {
-  char *path_env = getenv("PATH");
-  if (path_env == NULL) {
-    return NULL;
-  }
-
-  char *path_copy = strdup(path_env);
-  char *dir = strtok(path_copy, ":");
+char *find_in_path(const char *command, path_t *path) {
   static char full_path[1024];
-
-  while (dir != NULL) {
-    snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+  for (int i = 0; i < path->num_paths; i++) {
+    snprintf(full_path, sizeof(full_path), "%s/%s", path->paths[i], command);
     if (is_executable(full_path)) {
-      free(path_copy);
       return full_path;
     }
-    dir = strtok(NULL, ":");
   }
-  free(path_copy);
   return NULL;
 }
 
@@ -44,73 +83,74 @@ void fork_and_exec_cmd(char *full_path, int argc, char **argv) {
   }
 }
 
-int handle_input(char *input) {
-  char command[20];
-  sscanf(input, "%s", command);
-  if (!strcmp(command, "exit")) {
+int handle_input(cmd_t *cmd, path_t *path) {
+  if (strcmp(cmd->name, "exit") == 0) {
     int arg = 0;
-    sscanf(input, "%*s %d", &arg);
+    sscanf(cmd->args[0], "%d", &arg);
     exit(arg);
   }
-  if (!strcmp(command, "echo")) {
-    printf("%s\n", input + 5);
-    return 1;
+  if (strcmp(cmd->name, "echo") == 0) {
+    for (int i = 0; i < cmd->num_args; i++) {
+      printf("%s", cmd->args[i]);
+      if (i < cmd->num_args - 1) {
+        printf(" ");
+      }
+    }
+    printf("\n");
+    return 0;
   }
-  if (!strcmp(command, "pwd")) {
+  if (strcmp(cmd->name, "pwd") == 0) {
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
     printf("%s\n", cwd);
-    return 1;
+    return 0;
   }
-  if (!strcmp(command, "cd")) {
-    char *path = input + 3;
-    if (strcmp(path, "~") == 0) {
-      path = getenv("HOME");
+  if (strcmp(cmd->name, "cd") == 0) {
+    if (strcmp(cmd->args[0], "~") == 0) {
+      cmd->args[0] = getenv("HOME");
     }
-    if (chdir(path) == 0) {
-      return 1;
+    if (chdir(cmd->args[0]) == 0) {
+      return 0;
     } else {
-      printf("cd: %s: No such file or directory\n", path);
-      return 1;
+      printf("cd: %s: No such file or directory\n", cmd->args[0]);
+      return 0;
     }
   }
-  if (!strcmp(command, "type")) {
-    char arg[20];
-    sscanf(input, "%*s %s", arg);
-    if (!strcmp(arg, "exit") || !strcmp(arg, "echo") || !strcmp(arg, "type") ||
-        !strcmp(arg, "pwd")) {
-      printf("%s is a shell builtin\n", arg);
-      return 1;
+  if (strcmp(cmd->name, "type") == 0) {
+    if (!strcmp(cmd->args[0], "exit") || !strcmp(cmd->args[0], "echo") ||
+        !strcmp(cmd->args[0], "type") || !strcmp(cmd->args[0], "pwd")) {
+      printf("%s is a shell builtin\n", cmd->args[0]);
+      return 0;
     } else {
-      char *path = find_in_path(arg);
-      if (path) {
-        printf("%s is %s\n", arg, path);
-        return 1;
+      char *full_path = find_in_path(cmd->args[0], path);
+      if (full_path) {
+        printf("%s is %s\n", cmd->args[0], full_path);
+        return 0;
       } else {
-        printf("%s: not found\n", arg);
-        return 1;
+        printf("%s: not found\n", cmd->args[0]);
+        return 0;
       }
     }
   } else {
     char *argv[10];
-    int argc = 0;
-    char *token = strtok(input, " ");
-    while (token != NULL && argc < 10) {
-      argv[argc++] = token;
-      token = strtok(NULL, " ");
+    int argc = cmd->num_args + 1;
+    argv[0] = cmd->name;
+    for (int i = 1; i < cmd->num_args + 1; i++) {
+      argv[i] = cmd->args[i - 1];
     }
-    argv[argc] = NULL;
-    char *cmd_path = find_in_path(argv[0]);
+    char *cmd_path = find_in_path(argv[0], path);
     if (cmd_path) {
       fork_and_exec_cmd(cmd_path, argc, argv);
-      return 1;
-    } else {
       return 0;
+    } else {
+      return 1;
     }
   }
 }
 
 int main() {
+  path_t path;
+  parse_path(&path);
   while (true) {
     printf("$ ");
     fflush(stdout);
@@ -118,10 +158,11 @@ int main() {
     // Wait for user input
     char input[100];
     fgets(input, 100, stdin);
+    cmd_t cmd;
+    parse_command(input, &cmd);
 
-    input[strlen(input) - 1] = '\0';
-    if (!handle_input(input)) {
-      printf("%s: command not found\n", input);
+    if (handle_input(&cmd, &path) != 0) {
+      printf("%s: command not found\n", cmd.name);
     }
   }
   return 0;
